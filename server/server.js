70e067641e38b17761d9efccb0b9feaac78b2b02
Sanu Mondal <164723215+Sanu-2004@users.noreply.github.com>
2024-07-24 15:19:00 +0530

    added static file

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const peerServer = require('./socket/peerServer');
 
 const _dirname = path.resolve();
 
+
 peerServer();
 dotenv.config();
 const PORT = process.env.Port || 5000;
@@ -29,7 +30,7 @@ app.use(cors(
 ));
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
-
+app.use(express.static(path.join(_dirname, "client", "dist")));
 
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
@@ -38,11 +39,12 @@ app.use("/api/message", messaageRoute);
 app.use("/api/notification", notificatonRoute);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(_dirname, "/client/dist/index.html"));
+    res.sendFile(path.join(_dirname, "client", "dist", "index.html"));
 });
 
 
 server.listen(PORT, () => {
     connectDB();
-    console.log(`Server is running on port ${PORT}`);
+    // console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
